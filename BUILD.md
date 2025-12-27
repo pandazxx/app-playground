3b692ac3691fb679b25d7b7d08db96314545da41
xx <pandazxx@gmail.com>
2025-12-27 17:34:40 +0800

    Add poll and mqtt

---
+++ BUILD.md
@@ -0,0 +1,8 @@
+
+
+# Build
+
+```bash
+west build -b esp32s3_devkitc/esp32s3/procpu  . -DZEPHYR_EXTRA_MODULES="$(pwd)/../app-nvs-lib;$(pwd)/../app-bt-lib;$(pwd)/../app-wifi-lib;$(pwd)/../app-mqtt-lib;$(pwd)/../app-dns-lib;$(pwd)/../app-poll-lib"
+
+```
--- src/main.c
+++ src/main.c
@@ -1,45 +1,63 @@
+#include "zephyr/logging/log.h"
 #include "zephyr/logging/log_core.h"
+#include "zephyr/net/mqtt.h"
 #include "zephyr/sys/printk.h"
-#include <app_nvs_lib/app_nvs_lib.h>
-#include <stdint.h>
-#include <string.h>
+#include <app_dns_lib/app_dns_lib.h>
+#include <app_mqtt_lib/app_mqtt_lib.h>
+#include <app_poll_lib/app_poll_lib.h>
+#include <app_wifi_lib/app_wifi_lib.h>
 #include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(app_playground, LOG_LEVEL_DBG);
+static app_dns_record test_dns_record = {.resolved = false,
+                                         .host_name = "hcs01.local"};
 
-#define test_int_key1 1
-#define test_utf8_key2 2
-#define key_not_exist 3
+static struct mqtt_topic test_topic1 = {
+    .topic = MQTT_UTF8_LITERAL("sensor/test"),
+    .qos = MQTT_QOS_1_AT_LEAST_ONCE,
+};
 
 int main(void) {
+  app_wifi_lib_init();
+  app_dns_lib_init();
+  app_poll_lib_init();
 
-  app_nvs_lib_init();
-  int32_t result1 = app_nvs_read_int32(test_int_key1);
+  int result = app_wifi_lib_do_something(21);
+  printk("Result = %d\n", result);
+  wait_for_network();
+  printk("done waiting network\n");
+  // k_sleep(K_SECONDS(10));
+  struct sockaddr_in mqtt_addr;
+  memset(&mqtt_addr, 0, sizeof(mqtt_addr));
 
-  LOG_DBG("Result int 1: result=%d", result1);
-  result1++;
+  mqtt_addr.sin_family = AF_INET;
+  mqtt_addr.sin_port = htons(1883); // port
+  inet_pton(AF_INET, "10.10.10.123", &mqtt_addr.sin_addr);
 
-  app_nvs_write_int32(test_int_key1, result1);
+  app_mqtt_lib_init("just for test", &mqtt_addr);
+  LOG_DBG("waiting for mqtt");
 
-  char buf[64];
-  char readbuf[128];
-
-  snprintk(buf, sizeof(buf), "NO: %d", result1);
-
-  int rs = app_nvs_read_utf8(test_utf8_key2, readbuf, sizeof(readbuf));
-  readbuf[rs] = 0;
-  LOG_DBG("Read UTF8: %s", readbuf);
-
-  app_nvs_write_utf8(test_utf8_key2, buf, strlen(buf));
-
-  rs = app_nvs_read_utf8(key_not_exist, readbuf, sizeof(readbuf));
-
-  LOG_DBG("Read result for key_not_exist: rs=%d, errmsg=%s", rs,
-          strerror(abs(rs)));
-
+  // wait_for_mqtt();
+  // LOG_DBG("queryinng dns");
+  // app_dns_query(&test_dns_record);
+  //
+  // k_sleep(K_SECONDS(10));
+  //
+  // LOG_DBG("queryinng dns");
+  // app_dns_query(&test_dns_record);
+  //
+  // app_poll_start();
+  int test_topic_value = 0;
   while (1) {
-    k_msleep(1000);
+    app_poll_loop();
+    uint8_t mqtt_buf[256];
+
+    snprintk(mqtt_buf, sizeof(mqtt_buf), "test_topic1:  %d",
+             test_topic_value++);
+    app_mqtt_publish(test_topic1, (struct mqtt_binstr){
+                                      .data = mqtt_buf,
+                                      .len = strlen(mqtt_buf),
+                                  });
+    k_sleep(K_SECONDS(1));
   }
-  return 0;
 }
